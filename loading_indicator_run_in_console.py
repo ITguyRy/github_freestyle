b90acc9b3e7565be962446db4f813d9e19c80e8f
RyanU <rykung@gmail.com>
2019-07-24 16:34:33 -0700

    closer replication to the video, run it in the console

---
+++ loading_indicator_run_in_console.py
@@ -0,0 +1,32 @@
+#Ung, Ryan
+#Loading indicator replicating https://www.youtube.com/watch?v=bG-IDLfKIoo
+
+# importing time module just to slow down the code by a little.
+import time
+import os, sys
+
+
+def counter():
+    count = 0
+    dots = ['.', '..','...','....']
+    #starting at -1 to grab index 0 in the list at the end of first iteration
+    dots_count = -1
+    #100 is our goal we will loop til that is met
+    while 100 > count:    
+        count+=1
+        dots_count=dots_count+1
+        time.sleep(0.1)
+        #we only want to grab uptil index 3 so we will reset at 4 here
+        if dots_count == 4:
+            dots_count = -1
+        
+            print('                                                           ', end='\r', flush=True)
+        else:
+           # print("{}% complete. loading{}".format(count, dots[dots_count]))
+            print("{}% complete. loading{}".format(count, dots[dots_count]), end='\r')
+def main():
+    counter()
+    print('100% complete. loading.')
+    print('loading complete.')
+if __name__ == '__main__':
+    main()
